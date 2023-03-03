0a4aef165e3d3b2b0e0b059ed7082d0a5b9c4eee
Lisa Roehrig <122292818+lisa-lee-dev@users.noreply.github.com>
2023-03-03 10:06:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+//import React from "react";
+import PortraitGame from "./Components/PortraitGame.js";
+
+export default function App() {
+  return (
+    <div>
+      <h1>This is going to be a great Game!</h1>
+      <PortraitGame />
+    </div>
+  );
+}
---
+++ src/Components/PortraitGame.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PortraitGame = () => {
+  return (
+    <div>
+      <h1>Who is this?</h1>
+    </div>
+  );
+};
+
+export default PortraitGame;

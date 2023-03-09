118867fb9509f70f7124630d1ebfb6be606ecb8b
George Surmava <111701072+surmavagit@users.noreply.github.com>
2023-03-09 17:09:56 +0100

    The API is attached correctly

--- src/components/PortraitGame.js
+++ src/components/PortraitGame.js
@@ -2,22 +2,24 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import getData from "./PortraitWrapper.js";
+import { digestTheData } from "./PortraitWrapper.js";
 import Question from "./Question.js";
 
 const PortraitGame = () => {
   const [gameStage, setGameStage] = useState(0); //gameStage is a number, describing the number of questions already asked
-  const [data, setData] = useState([]);
-
+  const [data, setData] = useState([[{fullName: "full name testing", isCorrect: true}],[],[],[],[],[],[],[],[],[]]);
   useEffect(() => {
     (async function () {
-      setData(await getData());
+      const rawData = await getData();
+      console.log("data is imported");
+      setData(digestTheData(rawData));
     })();
-  });
+  }, [setData]);
   return (
     <>
       <h1>Here be points</h1>
       {gameStage < 10 ? (
-        <Question data={data} stage={gameStage} setter={setGameStage}  />
+        <Question data={data[gameStage]} stage={gameStage} setter={setGameStage}  />
       ) : (
         <h1>Here be results</h1>
       )}
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,14 @@
 import React from "react";
 import CharButton from "./CharButton.js"
 
-const Question = ({data}) => {
+const Question = ({data, stage, setter}) => {
   return (
     <>
       <h1>Who is this?</h1>
       <h1>Here be the picture</h1>
+      <img src = {data.filter(character => character.isCorrect)[0].imageUrl} />
       {data.map((character) => (
-        <CharButton charName={character.fullName} />
+        <CharButton name={character.fullName} stage={stage} setter={setter}/>
       ))}
     </>
   );
